e7ae9411d1ba668f92c901bde73b4ee4a3a8a158
Jeff Beauplan <jeffbeauplan@gmail.com>
2017-05-25 18:41:42 -0400

    commit

--- utilities.js
+++ utilities.js
@@ -29,6 +29,20 @@ module.exports = {
         }
         
     },
+    findBill: function(json){
+        const status = json[0].status
+        const payee = json[0].payee
+        const payment_date = json[0].payment_date
+        const payment_amount = json[0].payment_amount
+        const recurring_date= json[0].recurring_date
+        return ("Your " + payee + " bill is due on"+ payment_date+" in the amount of $"+ payment_amount+". This bill reoccurs on the "+ recurring_date +"st of every month")
+    },
+    payBill: function(json){
+        const status = json[0].status
+        const payee = json[0].payee
+        const payment_amount = json[0].payment_amount
+        return ("$"+payment_amount+" has been applied to your " + payee + " bill. Your balance due is now $0")
+    },
     replyToUser: function(request, response, assistant, speech) {
         if(request.body.originalRequest && request.body.originalRequest.source == "google") { //for google assistant
             assistant.ask(speech + ". What else can I help you with?"); //assistant.tell will end the conversation
